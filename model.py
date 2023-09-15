bccffe491be87a160cac03f4a531260b55d41f1f
sentense1 <3xsaint@gmail.com>
2023-09-15 11:03:42 +0100

    added docstrings

--- model.py
+++ model.py
@@ -2,16 +2,17 @@
 
 import os
 from flask_login import UserMixin
-import mysql.connector
-from mysql.connector import Error
+import psycopg2
+from psycopg2 import Error
 from werkzeug.security import check_password_hash
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-
 # Define the User class
+
+
 class User(UserMixin):
     """User class for managing user objects."""
 
@@ -34,26 +35,26 @@ class Database:
     def __init__(self):
         """Initialize the Database object."""
         try:
-            # Establish a connection to the MySQL database using environment variables
-            self.conn = mysql.connector.connect(
+            # Establish a connection to the PostgreSQL database using environment variables
+            self.conn = psycopg2.connect(
                 host=os.getenv('DB_HOST'),
+                database=os.getenv('DB_NAME'),
                 user=os.getenv('DB_USER'),
-                password=os.getenv('DB_PASSWORD'),
-                database=os.getenv('DB_NAME')
+                password=os.getenv('DB_PASSWORD')
             )
 
             # Check if the connection is successful
-            if self.conn.is_connected():
+            if self.conn:
                 # Create a cursor object for executing SQL queries
-                self.cursor = self.conn.cursor(dictionary=True)
+                self.cursor = self.conn.cursor()
         except Error as error:
             # Print an error message if connection fails
-            print("Error connecting to mySQL", error)
+            print("Error connecting to PostgreSQL", error)
 
     def close(self):
         """Close the database connection."""
         # Check if the database connection is open
-        if self.conn.is_connected():
+        if self.conn:
             # Close the cursor to release resources
             self.cursor.close()
             # Close the database connection itself
